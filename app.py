b2d54f5aeb266f4809c28fc031d2a73bc57f8d3e
ojizou003 <sinissi@gmail.com>
2024-10-06 17:33:02 +0900

    ✨ addForm

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Create a Flask Instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+# Create a Form Class
+class NameForm(FlaskForm):
+    name = StringField("Enter Your Name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 # Create a route decorator
 @app.route('/')
@@ -27,6 +40,18 @@ def page_not_found(e):
 def page_not_found(e):
     return render_template('500.html'), 500
 
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+
+    return render_template('name.html',
+                            name=name, 
+                            form=form
+                            )
 
 if __name__ == '__main__':
     app.run(debug=True)
