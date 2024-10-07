9b71363f0bdb72c53b73819d1b24f2184de99d4a
ojizou003 <sinissi@gmail.com>
2024-10-07 11:43:33 +0900

    connect-test-mysql

--- app.py
+++ app.py
@@ -16,7 +16,8 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 db = SQLAlchemy(app)
 
 class Users(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(200), nullable=False)
     email = db.Column(db.String(120), nullable=False, unique=True)
     date_added = db.Column(db.DateTime, default=datetime.now(timezone.utc))
